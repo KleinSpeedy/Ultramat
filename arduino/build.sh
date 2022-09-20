e2b1dd73cfd54463a1f76136009f73105135d1ac
jschulze <mail>
2022-09-20 18:01:16 +0200

    Delete arduino and move it to external Repository

--- arduino/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Build Arduino File for Flash
-arduino-cli compile -b arduino:avr:uno -p arduino.ino
--- arduino/flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Flash Arduino Ino Filo to Arduino Uno on ThinkPad
-
-arduino-cli upload -b arduino:avr:uno -p /dev/ttyACM0 -v
