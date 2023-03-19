c959276ddc8ee62a4920723158d9a50848818be5
jschulze <joni.schulze@t-online.de>
2023-03-19 20:30:34 +0100

    Make column enums globally visible so they can be used in callbacks

--- inc/drinklists.h
+++ inc/drinklists.h
@@ -7,6 +7,25 @@
 /* Main control for ingredients and recipes 
  * uses GTK Liststore for selection of drinks */
 
+// Column values for ingredients
+static enum {
+    ING_COLUMN_NAME,
+    ING_COLUMN_ID,
+    ING_COLUMN_POSITION,
+    ING_COLUMN_SELECTED,
+
+    ING_NUM_COLUMN
+}Ingredient_Columns;
+
+// Column values for recipes
+static enum {
+    REC_COLUMN_NAME,
+    REC_COLUMN_ID,
+    REC_COLUMN_AVAILABLE,
+
+    REC_NUM_COLUMNS
+}Recipe_Columns;
+
 GtkListStore *create_ingredient_listStore(Ing_Array_t *ingArray);
 
 GtkListStore *create_recipe_listStore(Rec_Array_t *recArray);
--- src/drinklists.c
+++ src/drinklists.c
@@ -15,17 +15,9 @@
 GtkListStore *create_ingredient_listStore(Ing_Array_t *ingArray)
 {
     // ingredient informations for list store 
-    enum {
-        COLUMN_NAME,
-        COLUMN_ID,
-        COLUMN_POSITION,
-        COLUMN_SELECTED,
-
-        NUM_COLUMN
-    };
     
     GtkListStore *ingListStore = GTK_LIST_STORE(gtk_list_store_new(
-        NUM_COLUMN,
+        ING_NUM_COLUMN,
         G_TYPE_STRING,
         G_TYPE_INT,
         G_TYPE_INT,
@@ -47,10 +39,10 @@ GtkListStore *create_ingredient_listStore(Ing_Array_t *ingArray)
         // new row
         gtk_list_store_append(ingListStore, &iterator);
         gtk_list_store_set(ingListStore, &iterator,
-            COLUMN_NAME, gname,
-            COLUMN_ID, gid,
-            COLUMN_POSITION, gposition,
-            COLUMN_SELECTED, gselected,
+            ING_COLUMN_NAME, gname,
+            ING_COLUMN_ID, gid,
+            ING_COLUMN_POSITION, gposition,
+            ING_COLUMN_SELECTED, gselected,
             -1); // terminate
     }
 
@@ -64,16 +56,9 @@ GtkListStore *create_ingredient_listStore(Ing_Array_t *ingArray)
 */
 GtkListStore *create_recipe_listStore(Rec_Array_t *recipeArray)
 {
-    enum {
-        COLUMN_NAME,
-        COLUMN_ID,
-        COLUMN_AVAILABLE,
-
-        NUM_COLUMNS
-    };
 
     GtkListStore *recipeListStore = gtk_list_store_new(
-        NUM_COLUMNS,
+        REC_NUM_COLUMNS,
         G_TYPE_STRING,
         G_TYPE_INT,
         G_TYPE_BOOLEAN);
@@ -91,9 +76,9 @@ GtkListStore *create_recipe_listStore(Rec_Array_t *recipeArray)
 
         gtk_list_store_append(recipeListStore, &iterator);
         gtk_list_store_set(recipeListStore, &iterator,
-            COLUMN_NAME, gname,
-            COLUMN_ID, gid,
-            COLUMN_AVAILABLE, gavailable,
+            REC_COLUMN_NAME, gname,
+            REC_COLUMN_ID, gid,
+            REC_COLUMN_AVAILABLE, gavailable,
             -1); // terminate 
     }
 
