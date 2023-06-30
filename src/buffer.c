f196a2daa0e988cd8769821c52a2927d3c5d4394
jschulze <joni.schulze@t-online.de>
2023-06-30 19:04:28 +0200

    Implement buffer creation functionality

--- src/buffer.c
+++ src/buffer.c
@@ -11,12 +11,23 @@
 Buffer_t *
 buffer_new(int size)
 {
-    
+    Buffer_t *buffer = (Buffer_t *) calloc(1, sizeof(Buffer_t));
+    buffer->read = 0;
+    buffer->write = 0;
+
+    buffer->size = size;
+    buffer->data = (uint8_t *) calloc(buffer->size, sizeof(uint8_t));
+
+    return buffer;
 }
 
 void buffer_destroy(Buffer_t *buf)
 {
+    if(buf == NULL)
+        return;
 
+    free(buf->data);
+    free(buf);
 }
 
 Buf_Status_t buffer_write_try(Buffer_t *buf, uint8_t *data, int count)
