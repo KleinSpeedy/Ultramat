668e30b4cdb3f46e1da717eb78effb1426e6014d
jschulze <joni.schulze@t-online.de>
2023-06-30 19:01:39 +0200

    Start implementing custom serial buffer

---
+++ src/buffer.c
@@ -0,0 +1,35 @@
+/**
+* Source implementation of custom serial buffer
+*/
+
+/* Module include */
+#include "buffer.h"
+
+/* System includes */
+#include <stdlib.h>
+
+Buffer_t *
+buffer_new(int size)
+{
+    
+}
+
+void buffer_destroy(Buffer_t *buf)
+{
+
+}
+
+Buf_Status_t buffer_write_try(Buffer_t *buf, uint8_t *data, int count)
+{
+
+}
+
+Buf_Status_t buffer_write_wait(Buffer_t *buf, uint8_t *data, int count)
+{
+
+}
+
+Buf_Status_t buffer_read(Buffer_t *buf, uint8_t *readBuf, int count)
+{
+
+}
