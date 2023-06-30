0a90ca31eb57ca102f403cdc92bc6c8dbc7a0cbe
jschulze <joni.schulze@t-online.de>
2023-06-30 19:32:46 +0200

    Add buffer read/write functionality
    Implement both blocking/non-blocking functionality.

--- inc/buffer.h
+++ inc/buffer.h
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include <stdint.h>
 
-typedef struct _buffer
+typedef struct buffer
 {
     pthread_mutex_t lock;   /* make buffer "thread safe" */
 
@@ -20,20 +20,22 @@ typedef struct _buffer
     int size;               /* actual buffer size */
 } Buffer_t;
 
-typedef enum _buf_status
+typedef enum buf_status
 {
     BUFFER_ERROR = -1,
-    BUFFER_WRITE_OK,    /* */
+    BUFFER_WRITE_OK,    /* Successfully written elements */
     BUFFER_READ_OK,     /* Successfully read elements */
-    BUFFER_FULL         /* Read == Write position */
+    BUFFER_FULL,        /* Read == Write position */
+    BUFFER_USED         /* Buffer is currently used */
 } Buf_Status_t;
 
 Buffer_t * buffer_new(int size);
 void buffer_destroy(Buffer_t *buf);
 
-Buf_Status_t buffer_write_try(Buffer_t *buf, uint8_t *data, int count);
-Buf_Status_t buffer_write_wait(Buffer_t *buf, uint8_t *data, int count);
+Buf_Status_t buffer_write_try(Buffer_t *buf, const uint8_t *data, int count);
+Buf_Status_t buffer_write_wait(Buffer_t *buf, const uint8_t *data, int count);
 
-Buf_Status_t buffer_read(Buffer_t *buf, uint8_t *readBuf, int count);
+Buf_Status_t buffer_read_try(Buffer_t *buf, uint8_t *readBuf, int count);
+Buf_Status_t buffer_read_wait(Buffer_t *buf, uint8_t *readBuf, int count);
 
 #endif //__ULTRAMAT_BUFFER_H__
