79f2f18ca260bc3a482a95a4cea7a22a8f94afab
jschulze <joni.schulze@t-online.de>
2023-07-01 10:00:57 +0200

    Add debug target and use GTK inspector for running debug target

--- run.sh
+++ run.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
 
-# run programm
-./build/src/ultramat
+#
+# helper script to run or debug gtk app
+#
+
+help()
+{
+    echo "Usage of $(basename $0) util script"
+    echo -e "\t" "-h" "\t" "prints this message"
+    echo -e "\t" "-d" "\t" "run debug gtk target"
+    echo -e "\t" "-r" "\t" "run release gtk target"
+}
+
+runTarget()
+{
+    WHERE=$1
+    ./build/$WHERE/ultramat
+}
+
+while getopts ":hrd" opt; do
+    case $opt in
+        h) # display Help
+            help
+            exit;;
+        r) # run release
+            runTarget release
+            exit;;
+        d) # run with debugging
+            GTK_DEBUG=interactive runTarget debug
+            exit;;
+    esac
+done
+
+echo "No option specified, try ./$(basename $0) -h"
