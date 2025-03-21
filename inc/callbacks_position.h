e9d6a907c842ae83554c8e6f9bb36b4b2c4c1fb3
jschulze <joni.schulze@t-online.de>
2025-03-21 17:57:04 +0100

    Add serial communication and adjust callback handlings to comms implementation

--- inc/callbacks_position.h
+++ inc/callbacks_position.h
@@ -22,36 +22,67 @@ typedef enum
     PAGES_COMBO_POS_NUM
 } ComboPositions_t;
 
+/* ========== CallbackID setter for GTK GUI ========== */
+
 /**
  * @brief Set callback ID of ingredient postion combo widgets
  */
 void cb_set_combo_position_callback_id(ComboPositions_t pos, uint64_t id);
+
+/**
+ * @brief Set callback ID of recipe order combo widget
+ */
+void cb_set_combo_order_callback_id(uint64_t id);
+// set recipe combo box widget
+void cb_set_combo_order_widget(GtkComboBox *widget);
+
+// motor control switch ID setter
+void cb_set_motor_switch_callback_id(uint64_t id);
+// setter to keep track of motor switch widget
+void cb_set_motor_switch_widget(GtkSwitch *widget);
+
+// recipe order button ID setter
+void cb_set_button_order_callback_id(uint64_t id);
+// setter to keep track of toggle button widget
+void cb_set_button_order_widget(GtkToggleButton *widget);
+// setter for progress bar widget
+void cb_set_progress_bar_widget(GtkProgressBar *widget);
+
+/* ========== GTK Widget event callbacks ========== */
 
 /*
  * @brief Combo box for ingredient position changed
  */
 void cb_on_combo_position_changed(GtkComboBox *cBox, void *data);
 
-/**
- * @brief Set callback ID of recipe order combo widget
- */
-void cb_set_combo_order_callback_id(uint64_t id);
-
 /*
  * @brief Combo box for ordered recipe changed
  */
 void cb_on_combo_order_changed(GtkComboBox *cBox, void *data);
 
-/* BUTTON - SWITCH CALLBACKS */
-
-void cb_set_motor_switch_callback_id(uint64_t id);
-
+/*
+ * @brief motor switch controlling serial connection to microcontroller
+ */
 int cb_on_motor_switch_toggle(GtkSwitch *motorSwitch, int state, void *data);
 
-void cb_set_button_order_callback_id(uint64_t id);
-
+/*
+ * @brief recipe order toggle button indicating start of drink mixture
+ */
 void cb_on_recipe_order_toggle(GtkToggleButton *button, void *data);
 
-ComboPositions_t cb_get_position_by_id(uint16_t id);
+/* ========== Callbacks for command done handlers ========== */
+
+// Setup command finished successfully, activate recipe ordering
+void cb_cmd_hello_there_done(void);
+
+// Recipe step move finished
+void cb_cmd_move_done(void);
+
+// error during serial communication, stop serial thread from GUI thread
+void cb_error_serial_communication(const char *str);
+
+/* Util functions */
+
+ComboPositions_t cb_get_position_by_id(const uint8_t id);
 
 #endif // CALLBACKS_POSITION_H
--- inc/serialcomms.h
+++ inc/serialcomms.h
@@ -2,9 +2,17 @@
 #define SERIALCOMMS_H
 
 #include "drinks.h"
+#include "message_handler.h"
+
+// Maximum size of a buffer in bytes is the maximum payload size + 6
+#define COMMS_MAX_BUFFER_SIZE CMD_BUFFER_SIZE + 6
 
 int comms_start_new_mixing(Recipe *rec);
 
+int comms_send_msg(const cmd_msg_t *msg);
+
 int comms_start_serial_connection(void);
+
+void comms_stop_serial_connection(void);
 
 #endif // SERIALCOMMS_H
